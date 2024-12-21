3df62f5477dee473f3b90eaa9e1e97d419c32510
jaxfloyd <jaxfloyd@gmail.com>
2024-12-20 19:00:50 -0500

    Initial commit

--- routes/test.js
+++ routes/test.js
@@ -1,5 +1,6 @@
 const setupTestRoute = ( router ) => {
     router.get('/test', (req, res) => {
+        console.log("The 'test' endpoint was reached.")
         res.status(200).json({
           success: true,
           message: 'Server is reachable!',
