6cf537745e0ab059bb8fa874bd31228b3a6b6432
jaxfloyd <jaxfloyd@gmail.com>
2025-01-26 13:34:20 -0500

    Initial commit

--- routes/data/writeZip.js
+++ routes/data/writeZip.js
@@ -4,19 +4,19 @@ const path = require('path');
 const filePath = path.join(__dirname, '../../zips.txt');
 
 const setupWriteZip = (router) => {
-    router.post('/write-zip', async (req, res) => {
-        console.log("'/write-zip' endpoint was reached.");
-        try {
-            console.log(req.body)
-            // Write the  back to the file
-            await fs.promises.writeFile(filePath, req.body.zip.toString(), 'utf8');
-            return res.json({ status: 'success' });
-            
-        } catch (error) {
-            console.error('Error writing zip:', error);
-            res.status(500).json({ error: 'Failed to write zip' });
-        }
-    });
+  router.post('/write-zip', async (req, res) => {
+    console.log("'/write-zip' endpoint was reached.");
+    try {
+      console.log(req.body); // Logs the body for debugging
+      // Write the ZIP to the file
+      await fs.promises.writeFile(filePath, req.body.zip.toString(), 'utf8');
+      return res.json({ status: 'success' });
+      
+    } catch (error) {
+      console.error('Error writing zip:', error);
+      res.status(500).json({ error: 'Failed to write zip' });
+    }
+  });
 };
 
 module.exports = setupWriteZip;
