997f711960ae7ed2468c60c3dd3cb1ce5ce6864a
jaxfloyd <jaxfloyd@gmail.com>
2025-03-27 07:03:24 -0400

    Initial commit

--- routes/stripe/listCharges.js
+++ routes/stripe/listCharges.js
@@ -1,18 +1,19 @@
 const stripe = require('../../configs/stripe.js'); // Import Stripe configuration
 
 const setupListCharges = (router) => {
-  // Route to handle listing charges
   router.post('/list-charges', async (req, res) => {
     console.log("'/list-charges' endpoint was reached.");
     try {
-      const { limit = 100 } = req.body;
+      const { limit = 100, starting_after } = req.body;
 
       const charges = await stripe.charges.list({
-        limit
+        limit,
+        ...(starting_after && { starting_after }) // only add if defined
       });
 
       res.status(200).json({
-        charges: charges.data
+        charges: charges.data,
+        has_more: charges.has_more, // include this for pagination logic on the client
       });
 
     } catch (error) {
