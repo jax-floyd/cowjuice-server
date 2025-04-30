9a6470e213ba6cd0cb335892bf0c6baba3b591a1
jaxfloyd <jaxfloyd@gmail.com>
2025-04-30 19:39:58 -0400

    Initial commit

--- routes/stripe/createPaymentIntent.js
+++ routes/stripe/createPaymentIntent.js
@@ -13,7 +13,7 @@ const setupCreatePaymentIntentRoute = (router) => {
 
       // We're creating payment intents with all user data. In theory, this is for confirmation of Shopify information.
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount / 100, // cents
+        amount: amount, // <-- comes in as 3599 and needs to be 3599 for successful payment intent creation
         currency: 'usd',
         receipt_email: email,
         metadata: {
