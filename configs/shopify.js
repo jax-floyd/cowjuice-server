edc0c1836536c56c9539baeaf2243fca4774d8bb
jaxfloyd <jaxfloyd@gmail.com>
2025-05-31 20:20:34 -0400

    Initial commit

--- configs/shopify.js
+++ configs/shopify.js
@@ -8,7 +8,7 @@ dotenv.config();
 const shopify = {
   storefrontToken: process.env.STOREFRONT_TOKEN,
   adminToken: process.env.ADMIN_TOKEN,
-  storeDomain: process.env.STORE_DOMAIN, // e.g., your-store.myshopify.com
+  storeDomain: 'cow-juice.myshopify.com', // e.g., your-store.myshopify.com
   apiVersion: '2023-10', // You can update this to the latest supported version
 };
 
