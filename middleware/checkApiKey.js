ab2add034c2547e977a6176a33d469114b0d2af6
jaxfloyd <jaxfloyd@gmail.com>
2025-06-04 21:34:15 -0400

    Initial commit

--- middleware/checkApiKey.js
+++ middleware/checkApiKey.js
@@ -3,7 +3,7 @@ require('../configs/loadEnv')(); // or dotenv.config()
 const checkApiKey = (req, res, next) => {
   const key = req.headers['x-api-key'];
   if (!key || key !== process.env.COWJUICE_API_KEY) {
-    return res.status(403).json({ error: 'Forbidden: Invalid or missing API key' });
+    return res.status(403).json({ error: 'Forbidden, like the applesauce: Invalid or missing API key' });
   }
   next();
 };
